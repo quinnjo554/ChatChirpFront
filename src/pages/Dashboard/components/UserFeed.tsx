572e67b128bae1344d63a7f88894c4ca24ed41c5
quinn <quinnjohnson24@gmail.com>
2023-09-15 19:52:05 -0500

    added user context

--- src/pages/Dashboard/components/UserFeed.tsx
+++ src/pages/Dashboard/components/UserFeed.tsx
@@ -1,9 +1,14 @@
+"use client"
+import { useGlobalContext } from '@/contexts/UserContext'
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 function UserFeed() {
+  const {name,email,image} = useGlobalContext()
   return (
+
   <Box>
+    <p>{email}</p>
   </Box>
   )
 }
